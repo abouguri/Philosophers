2064149f224b678b397aa69eba909754bf7259e0
XsineData <sin3ibad1705@gmail.com>
2024-12-01 23:21:15 +0100

    init simulation

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: rukia <rukia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 13:30:55 by abouguri          #+#    #+#             */
-/*   Updated: 2024/12/01 18:59:25 by rukia            ###   ########.fr       */
+/*   Updated: 2024/12/01 20:32:05 by rukia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,10 +105,197 @@ void    display_usage(void)
     printf("  nb_each_philo_must_eat (Optional): 0+\n");
 }
 
-// int initialize_simulation(int ac, char **av)
-// {
+int malloc_data(t_data *data)
+{
+    data->philos = malloc(sizeof(t_philo) * data->nb_philos);
+    if (data->philos == NULL)
+		return (ERROR_MALLOC_FAILURE);
+    data->forks = malloc(sizeof(pthread_mutex_t) * data->nb_philos);
+    if (data->forks == NULL)
+		return (free(data->philos), ERROR_MALLOC_FAILURE);
+    data->philo_threads = malloc(sizeof(pthread_t) * data->nb_philos);
+    if (data->philo_threads == NULL)
+		return (free(data->philos), free(data->forks), ERROR_MALLOC_FAILURE);
+    return (0);
+}
+
+int initialize_data(t_data  *data,int ac,char **av)
+{
+    data->nb_full_p = 0;
+    data->keep_iterating = true;
+    data->nb_philos = ft_atoi(av[1]);
+    data->die_time = (uint64_t)ft_atoi(av[2]);
+    data->eat_time = (uint64_t)ft_atoi(av[3]);
+    data->sleep_time = (uint64_t)ft_atoi(av[4]);
+    data->nb_meals = -1;
+
+    if (ac == 6)
+        data->nb_meals = ft_atoi(av[5]);
+    pthread_mutex_init(&data->mutex_eat_time, NULL);
+    pthread_mutex_init(&data->mutex_sleep_time, NULL);
+    pthread_mutex_init(&data->mutex_die_time, NULL);
+    pthread_mutex_init(&data->mutex_print, NULL);
+    pthread_mutex_init(&data->mutex_nb_philos, NULL);
+    pthread_mutex_init(&data->mutex_keep_iter, NULL);
+    pthread_mutex_init(&data->mutex_start_time, NULL);
+    return (malloc_data(data));
+}
+
+void    cleanup_philosophers(t_philo *philos, int count)
+{
+    int i;
+
+    i = 0;
+    while (i < count)
+    {
+        pthread_mutex_destroy(&philos[i].mutex_state);
+        pthread_mutex_destroy(&philos[i].mutex_nb_meals_had);
+        pthread_mutex_destroy(&philos[i].mutex_last_eat_time);
+        i++;
+    }
+}
+
+uint64_t	get_time(void)
+{
+	struct timeval	tv;
+
+	if (gettimeofday(&tv, NULL))
+		return (0);
+	return ((tv.tv_sec * (uint64_t)1000) + (tv.tv_usec / 1000));
+}
+
+void update_last_meal_time(t_philo *philo)
+{
+    pthread_mutex_lock(&philo->mutex_last_eat_time);
+    philo->last_eat_time = get_time(); // Assume get_time() returns the current timestamp
+    pthread_mutex_unlock(&philo->mutex_last_eat_time);
+}
+
+
+int initialize_philosopher_mutexes(t_philo *philo)
+{
+    if (pthread_mutex_init(&philo->mutex_state, NULL) != 0)
+        return (ERROR_MUTEX_INIT);
+    if (pthread_mutex_init(&philo->mutex_nb_meals_had, NULL) != 0)
+    {
+        pthread_mutex_destroy(&philo->mutex_state);
+        return (ERROR_MUTEX_INIT);
+    }
+    if (pthread_mutex_init(&philo->mutex_last_eat_time, NULL) != 0)
+    {
+        pthread_mutex_destroy(&philo->mutex_state);
+        pthread_mutex_destroy(&philo->mutex_nb_meals_had);
+        return (ERROR_MUTEX_INIT);
+    }
+    return (SUCCESS);
+}
+
+int initialize_philos(t_data *data)
+{
+    t_philo *philos;
+    int i;
+
+    i = 0;
+    philos = data->philos;
+    while (i < data->nb_philos)
+	{
+        philos[i].data = data;           // Link to global simulation data
+        philos[i].id = i + 1;           // Assign philosopher ID (1-based)
+        philos[i].nb_meals_had = 0;     // Initialize meals eaten count
+        philos[i].state = IDLE;         // Set initial state to IDLE
+        
+		if (initialize_philosopher_mutexes(&philos[i]) != SUCCESS)
+        {
+            cleanup_philosophers(philos, i); // Free initialized mutexes
+            return (ERROR_MUTEX_INIT);
+        }
+		update_last_meal_time(&philos[i]);
+        i++;
+	}
+    return (SUCCESS);
+}
+
+void cleanup_forks(t_data *data, int count)
+{
+    int i;
+
+    i = 0;
+    while (i < count)
+    {
+        pthread_mutex_destroy(&data->forks[i]);
+        i++;
+    }
+}
+
+void assign_forks(t_data *data)
+{
+    int i;
+    t_philo *philos = data->philos;
+
+    i = 0;
+    philos[0].left_fork = &data->forks[0];
+    philos[0].right_fork = &data->forks[data->nb_philos - 1];
+
+    while (++i < data->nb_philos)
+    {
+        philos[i].left_fork = &data->forks[i];
+        philos[i].right_fork = &data->forks[i - 1];
+    }
+}
+
+int initialize_forks(t_data *data)
+{
+    int i;
+
+    i = 0;
+    // Initialize mutexes for forks
+    while (i < data->nb_philos)
+    {
+        if (pthread_mutex_init(&data->forks[i], NULL) != 0)
+        {
+            cleanup_forks(data, i);
+            return (ERROR_MUTEX_INIT);
+        }
+        i++;
+    }
+    assign_forks(data);
+    return (SUCCESS);
+}
+
+int	philos_count(t_data *data)
+{
+	int	nb_philos;
+
+	pthread_mutex_lock(&data->mutex_nb_philos);
+	nb_philos = data->nb_philos;
+	pthread_mutex_unlock(&data->mutex_nb_philos);
+	return (nb_philos);
+}
+
+int start_threads(t_data *data)
+{
+    int i;
+    int nb_of_philos;
+
+    nb_of_philos = get_nb_philos(data); // Get the number of philosophers
+    data->start_time = get_time();
+    i = 0;
     
-// }
+}
+
+int initialize_simulation(int ac, char **av)
+{
+    t_data data;
+    
+    if (initialize_data(&data, ac, av) != 0)
+        return (ERROR_MALLOC_FAILURE);
+    initialize_philos(&data);
+    initialize_forks(&data);
+    start_threads(&data);
+    // wait_for_threads(&data);
+    // cleanup_simulation(&data);
+    return (SUCCESS);
+}
 
 int main(int ac, char **av)
 {
@@ -117,7 +304,7 @@ int main(int ac, char **av)
 		display_usage();
 		return (ERROR_WRONG_INPUT);
 	}
-	// if (initialize_simulation(ac, av) != 0)
-	// 	return (ERROR_MALLOC_FAILURE);
+	if (initialize_simulation(ac, av) != 0)
+		return (ERROR_MALLOC_FAILURE);
     return (SUCCESS);
 }
--- philosophers.h
+++ philosophers.h
@@ -13,8 +13,77 @@
 # define SUCCESS 0
 # define ERROR_WRONG_INPUT 1
 # define ERROR_MALLOC_FAILURE 2
+# define ERROR_MUTEX_INIT 3
 
 # define INT_MIN -2147483648
 # define INT_MAX 2147483647
+
+typedef enum e_philo_state
+{
+    EATING = 0,   // Philosopher is eating
+    SLEEPING = 1, // Philosopher is sleeping
+    THINKING = 2, // Philosopher is thinking
+    DEAD = 3,     // Philosopher has died
+    FULL = 4,     // Philosopher is full (has eaten required meals)
+    IDLE = 5      // Philosopher is idle (e.g., before starting or waiting)
+} t_state;
+
+typedef struct s_philo
+{
+    // Philosopher-specific data
+    int             id;                 // Philosopher ID
+    int             nb_meals_had;       // Number of meals eaten
+    uint64_t        last_eat_time;      // Timestamp of the last meal (in ms)
+
+    // Pointer to shared simulation data
+    struct s_data   *data;              // Pointer to global simulation data
+
+    // State management
+    t_state         state;              // Current state (e.g., THINKING, EATING, SLEEPING)
+    pthread_mutex_t mutex_state;        // Protects the philosopher's state
+
+    // Forks
+    pthread_mutex_t *left_fork;         // Pointer to the left fork mutex
+    pthread_mutex_t *right_fork;        // Pointer to the right fork mutex
+
+    // Synchronization
+    pthread_mutex_t mutex_nb_meals_had; // Protects nb_meals_had
+    pthread_mutex_t mutex_last_eat_time; // Protects last_eat_time
+} t_philo;
+
+
+typedef struct s_data
+{
+    // Simulation parameters
+    int             nb_philos;         // Number of philosophers
+    int             nb_meals;          // Number of meals each philosopher must eat
+    int             nb_full_p;         // Number of philosophers who have finished eating
+    bool            keep_iterating;    // Flag to keep the simulation running
+
+    // Timing parameters (in milliseconds)
+    uint64_t        eat_time;          // Time it takes for a philosopher to eat
+    uint64_t        die_time;          // Time before a philosopher dies without eating
+    uint64_t        sleep_time;        // Time a philosopher spends sleeping
+    uint64_t        start_time;        // Simulation start time
+
+    // Synchronization primitives (mutexes)
+    pthread_mutex_t mutex_eat_time;    // Protects eat_time
+    pthread_mutex_t mutex_die_time;    // Protects die_time
+    pthread_mutex_t mutex_sleep_time;  // Protects sleep_time
+    pthread_mutex_t mutex_print;       // Ensures atomic printing
+    pthread_mutex_t mutex_nb_philos;   // Protects nb_philos
+    pthread_mutex_t mutex_keep_iter;   // Protects keep_iterating flag
+    pthread_mutex_t mutex_start_time;  // Protects start_time
+
+    // Threads
+    pthread_t       monitor_all_alive; // Thread monitoring philosophers' survival
+    pthread_t       monitor_all_full;  // Thread monitoring philosophers' meal completion
+    pthread_t       *philo_threads;    // Array of philosopher threads
+
+    // Philosopher-related data
+    pthread_mutex_t *forks;            // Array of mutexes representing forks
+    t_philo         *philos;           // Array of philosopher data structures
+} t_data;
+
 
 #endif

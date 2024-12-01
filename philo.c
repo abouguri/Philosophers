8c7cf07be41c21a6c760673815e9b83a90f88874
XsineData <sin3ibad1705@gmail.com>
2024-12-01 17:59:43 +0100

    validate args

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: rukia <rukia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 13:30:55 by abouguri          #+#    #+#             */
-/*   Updated: 2024/12/01 17:52:56 by rukia            ###   ########.fr       */
+/*   Updated: 2024/12/01 17:59:10 by rukia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int validate_arguments(ac, av)
+int validate_arguments(int ac,char  **av)
 {
-
+    if (ac < 5 || ac > 6)
+		return (ERROR_WRONG_INPUT);
+	if (is_input_numeric(ac, av) != 0)
+		return (ERROR_WRONG_INPUT);
+	if (wrong_input_check(ac, av))
+		return (ERROR_WRONG_INPUT);
+	return (SUCCESS);
 }
 
 void    display_help_message()
